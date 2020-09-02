2c38eddf6d9e8a6f1946aac9c3668912a2f42e45
Mohammad Ravand <githubmohammadrepo@gmail.com>
2020-09-02 21:31:40 +0430

    add spinner for loading contnet from server

--- store/resume.js
+++ store/resume.js
@@ -10,25 +10,29 @@ export const mutations = {
   },
   toggleRealDialog(state) {
     state.realDialog = !state.realDialog
-  }
+  },
 }
 // actions
 export const actions = {
-  getImages(context){
+  getImages(context) {
+
     return this.$axios.$get('https://jsonplaceholder.typicode.com/photos')
-    .then(res => {
-      context.images=res;
-      console.log(context.images)
-      return res;
-    })
-    .catch(err => {
-      console.log(err)
-    })
+      .then(res => {
+        context.images = res.filter(data => {
+          return res.indexOf(data) < 100;
+        })
+        return context.images;
+      })
+      .catch(err => {
+        context.images = [{ id: Math.random(), title: "هیچ پروژه ای یافت نشد" }];
+        // eslint-disable-next-line no-console
+        console.log(err)
+      })
   }
 }
 // getters
 export const getters = {
-  getTenImages(state){
+  getTenImages(state) {
     return state.images;
   }
 }
